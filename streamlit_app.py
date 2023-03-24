9f8f601cfd4544ca1b5470604d114be44f8aa1b4
fmister <126615059+fmister@users.noreply.github.com>
2023-03-24 14:55:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
-#import streamlit
+import streamlit
 import snowflake.connector
-#streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title('My Parents New Healthy Diner')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
